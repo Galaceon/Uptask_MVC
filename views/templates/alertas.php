e2db354610e151ba8490ef45cce1eb2bccfaf6e0
Galaceon <antoniogarcialtr@gmail.com>
2025-11-07 02:18:02 +0100

    CSS for error create account alerts

--- views/templates/alertas.php
+++ views/templates/alertas.php
@@ -1,8 +1,6 @@
 <?php 
     foreach($alertas as $key => $alerta):
-        foreach($alerta as $mensaje):
-            debuguear($key);
-        ?>
+        foreach($alerta as $mensaje):?>
 
             <div class="alerta <?php echo $key; ?>"><?php echo $mensaje; ?></div>
 
