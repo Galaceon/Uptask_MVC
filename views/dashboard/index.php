b43863bd7a463ed4a9b09bb5c497e9a6abcb216c
Galaceon <antoniogarcialtr@gmail.com>
2025-11-14 19:44:16 +0100

    listing all user projects

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,3 +1,18 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
+    <?php if(count($proyectos) === 0) { ?>
+        <p class="no-proyectos">No hay Proyectos aún</p>
+        <a href="/crear-proyecto" class="crear-proyecto">Crea tu primer Proyecto</a>
+    <?php } else { ?>
+        <ul class="listado-proyectos">
+            <?php foreach($proyectos as $proyecto) { ?>
+                <li class="proyecto">
+                    <a href="/proyecto?url=<?php echo $proyecto->url; ?>">
+                        <?php echo $proyecto->proyecto ?>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
+
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
