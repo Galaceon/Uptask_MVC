562c85c97ccedb77334b04ed46cbdea822aae270
Galaceon <antoniogarcialtr@gmail.com>
2025-11-14 22:05:49 +0100

    adding JS to show the form

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function() {
+    // Boton para mostrar el modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        console.log('mostrando...');
+    }
+})();
