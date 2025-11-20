2d46f0524f97e359d30c1c68ac75d7ac57a5fa5f
Galaceon <antoniogarcialtr@gmail.com>
2025-11-20 18:27:08 +0100

    continuing scripting and build IU

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -30,6 +30,10 @@
             return;
         }
         
+        const estados = {
+            0: 'Pendiente',
+            1: 'Completa'
+        }
         tareas.forEach(tarea => {
             const contenedorTarea = document.createElement('LI');
             contenedorTarea.dataset.tareaId = tarea.id;
@@ -38,8 +42,20 @@
             const nombreTarea = document.createElement('P');
             nombreTarea.textContent = tarea.nombre;
 
+            const opcionesDiv = document.createElement('DIV');
+            opcionesDiv.classList.add('opciones');
+            
+            // Botones
+            const btnEstadoTarea = document.createElement('BUTTON');
+            btnEstadoTarea.classList.add('estado-tarea');
+            btnEstadoTarea.classList.add(`${estados[tarea.estado].toLowerCase()}`);
+            btnEstadoTarea.textContent = estados[tarea.estado];
+            btnEstadoTarea.dataset.estadoTarea = tarea.estado;
+
+
+
             console.log(tarea);
-            console.log(nombreTarea);
+            console.log(btnEstadoTarea);
 
         })
     }
