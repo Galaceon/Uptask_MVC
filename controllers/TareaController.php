204c15cdfced722a0a8a8db71f26008cf6f19413
Galaceon <antoniogarcialtr@gmail.com>
2025-11-22 22:27:38 +0100

    delete tasks finished

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -96,7 +96,32 @@ class TareaController {
     
     public static function eliminar() {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Validar que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['proyectoId']);
+            session_start();
+
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error al agregar la tarea',
+                    'animacion' => true
+                ];
+                echo json_encode($respuesta);
+                return;
+            };
+
+            $tarea = New Tarea($_POST);
+            $resultado = $tarea->eliminar();
+
+            $resultado = [
+                'resultado' => $resultado,
+                'mensaje' => 'Eliminado Correctamente',
+                'tipo' => 'exito',
+                'animacion' => true
+            ];
             
+            echo json_encode($resultado);
+
         }
     }
 }
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -293,9 +293,39 @@
     }
 
     async function eliminarTarea(tarea) {
+        // Detructuring del objeto tarea
+        const {estado, id, nombre, proyectoId} = tarea;
+        
         const datos = new FormData();
+        datos.append('id', id);
+        datos.append('nombre', nombre);
+        datos.append('estado', estado);
+        datos.append('proyectoId', obtenerProyecto());
 
         try {
+            const url = 'http://localhost:3000/api/tarea/eliminar';
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+            })
+
+            const resultado = await respuesta.json();
+            
+            if(resultado.resultado === true) {
+                Swal.fire({
+                    title: 'Eliminado!',
+                    text: resultado.mensaje,
+                    icon: 'success',
+                    width: '42rem',
+                    customClass: {
+                        popup: 'swal-grande',
+                        confirmButton: 'btn-grande',
+                    }
+                });
+
+                tareas = tareas.filter(tareaMemoria => tareaMemoria.id !== tarea.id);
+                mostrarTareas();
+            }
 
         } catch(error) {
             console.log(error);
