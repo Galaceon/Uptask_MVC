849cce4706ad9edcf63f43fe9aeef6d730e861ac
Galaceon <antoniogarcialtr@gmail.com>
2025-11-27 08:51:31 +0100

    Show/Hide MobileMenu/Sidebar

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,8 +1,1 @@
-const mobileMenuBtn = document.querySelector('#mobile-menu');
-const sidebar = document.querySelector('.sidebar');
-
-if(mobileMenuBtn) {
-    mobileMenuBtn.addEventListener('click', function() {
-        sidebar.classList.toggle('mostrar')
-    })
-}
+const mobileMenu=document.querySelector("#mobile-menu"),cerrarMenu=document.querySelector("#cerrar-menu"),sidebar=document.querySelector(".sidebar");mobileMenu&&mobileMenu.addEventListener("click",function(){sidebar.classList.add("mostrar")}),cerrarMenu&&cerrarMenu.addEventListener("click",function(){sidebar.classList.add("ocultar"),setTimeout(()=>{sidebar.classList.remove("mostrar"),sidebar.classList.remove("ocultar")},400)});
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,5 +1,12 @@
 <aside class="sidebar">
-    <h2><a href="/dashboard">UpTask</a></h2>
+    <div class="contenedor-sidebar">
+        <h2><a class="title-link" href="/dashboard">UpTask</a></h2>
+
+        <div class="cerrar-menu">
+            <img id="cerrar-menu" src="build/img/cerrar.svg" alt="imagen cerrar menu">
+        </div>
+    </div>
+    
 
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : '' ?>" href="/dashboard">Proyectos</a>
